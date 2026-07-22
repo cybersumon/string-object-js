df10bb7eb677810bc35b4c3db9402c0aadd2d2e5
cybersumon <jsrsumon@gmail.com>
2026-07-22 16:31:28 +0600

    first commit

---
+++ string-array.js
@@ -0,0 +1,14 @@
+let str = "Hello";
+let arry = ['H','E','L','L','O'];
+
+console.log(str.length);
+console.log(arry.length)
+
+console.log(str[4])
+console.log(arry[4])
+
+let addStr = str.push('!')
+let addArry = arry.push('!')
+
+console.log(addStr)
+console.log(addArry)
