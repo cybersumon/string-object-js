526ee480cb96775ceae41d337020a434b3e6f47d
cybersumon <jsrsumon@gmail.com>
2026-07-22 20:50:08 +0600

    first commit

--- string-array.js
+++ string-array.js
@@ -1,14 +1,41 @@
-let str = "Hello";
-let arry = ['H','E','L','L','O'];
+// let str = "Hello";
+// let arry = ['H','E','L','L','O'];
 
-console.log(str.length);
-console.log(arry.length)
+// console.log(str.length);
+// console.log(arry.length)
 
-console.log(str[4])
-console.log(arry[4])
+// console.log(str[4])
+// console.log(arry[4])
+// str.push('!')
+// arry.push('!')
+// console.log(str)
+// console.log(arry)
 
-let addStr = str.push('!')
-let addArry = arry.push('!')
+// let string = "javaScript"
+// let abc = string.split('');
+// console.log(abc);
+// console.log(string.split(""));
+// console.log(string.concat(''))
+// console.log(typeof abc)
+// console.log(Array.isArray(abc))
 
-console.log(addStr)
-console.log(addArry)
+
+// let name = "JavaScRipt";
+// let text = name.toUpperCase()
+// console.log(text);asd
+
+// let smne = "Az ApPareLs";
+// let zxy = smne.toLowerCase();
+// console.log(zxy);
+
+// let hello = "  sumon   ";
+// console.log(hello);
+// let abc = hello.trim();
+// console.log(abc);
+
+let sentence = "learning javascript is fun!"
+let someword = sentence.slice(0,19);
+console.log(someword);
+let fun = sentence.slice(-5);
+console.log(fun)
+
